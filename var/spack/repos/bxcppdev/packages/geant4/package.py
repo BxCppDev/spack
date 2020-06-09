ee652064dcf2d83f323f189cd15e2ca783e23fde
Francois Mauger <mauger@lpccaen.in2p3.fr>
2020-06-09 09:45:57 +0200

    Add checksum to geant4 patches

--- var/spack/repos/bxcppdev/packages/geant4/package.py
+++ var/spack/repos/bxcppdev/packages/geant4/package.py
@@ -104,8 +104,10 @@ class Geant4(CMakePackage):
     patch('cxx17_geant4_10_0.patch', level=1, when='@10.4.0 cxxstd=17')
     patch('geant4-10.4.3-cxx17-removed-features.patch',
           level=1, when='@10.4.3 cxxstd=17')
-    patch('geant4-9.6.4-1.patch', level=1, when='@9.6.4')
-    patch('geant4-9.6.4-2.patch', level=1, when='@9.6.4')
+    patch('geant4-9.6.4-1.patch', level=1, when='@9.6.4',
+          sha256='8ce94a4b3a8108435737a1bbf296fcf4a9a8befbf30074ba62fe248079351448')
+    patch('geant4-9.6.4-2.patch', level=1, when='@9.6.4',
+          sha256='c8d232b0fc6f978115d7bb2a5f803b1dadea5fd97103ae60866a4dbb7af11bff')
 
     def url_for_version(self, version):
         """Handle version string."""
